75bc7a129cdb1bf8c7a29037174002f75021538d
mali5820k <mali5820k@gmail.com>
2025-04-29 03:12:09 -0400

    Show gui and show tree dev-scripts

---
+++ compiler-dev/frontend/show_tree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GRAMMAR_FILE=$1
+RULE=$2
+CLYTH_FILE=$3
+
+if [[ $# < 3 ]]; then
+    echo "Need three args for viewing tree: grammar-file rule clythfile"
+    exit 1
+fi
+
+antlr4-parse $GRAMMAR_FILE $RULE -tree $CLYTH_FILE
