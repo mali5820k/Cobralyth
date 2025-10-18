51a75f32f2dddc15ae087840fa14afa623000a3b
mali5820k <mali5820k@gmail.com>
2025-10-18 04:31:17 -0400

    Transition to Zig for C and C++ static compiles
    This resolves the need for creating a custom toolchain to compile llvm with musl-libc as the target. Ultimately allowing any produced binary to run on any system without reliance on glibc or libstdc++, instead opting for musl-libc and libc++ that're MIT and Apache License v2.0 with LLVM Exceptions.
    
    Still a linker error from a --dependency-file arg, but will fix with followup commits.

--- build_compiler.sh
+++ build_compiler.sh
@@ -3,12 +3,12 @@ CUR_DIR=$(dirname $0)
 cd $CUR_DIR
 CUR_DIR=$(pwd)
 
-# Confirm clang++, cmake, make, and libcxx (libc++) are all installed:
+# Confirm cmake, make, and zig are installed:
 MISSING=0
-clang++ --version
-CLANG_PLUS_PLUS=$?
-if [[ $CLANG_PLUS_PLUS != 0 ]]; then
-    printf "Missing clang++ install!\n"
+zig version
+ZIG=$?
+if [[ $ZIG != 0 ]]; then
+    printf "Missing zig install!\n"
     MISSING=1
 fi
 
@@ -26,60 +26,32 @@ if [[ $MAKE != 0 ]]; then
     MISSING=1
 fi
 
-podman --version
-podman=$?
+dnf list llvm-dev* --installed
+LLVM_DEVEL=$?
+if [[ $LLVM_DEVEL != 0 ]]; then
+    printf "Missing llvm-devel install!\n"
+    MISSING=1
+fi
 
-if [[ $podman != 0 ]]; then
-    printf "Missing podman installation!"
+dnf list llvm --installed
+LLVM=$?
+if [[ $LLVM != 0 ]]; then
+    printf "Missing llvm install!\n"
     MISSING=1
 fi
 
 # Exit out if packages are missing, since the script is NOT meant to install them for you.
 if [[ $MISSING == 1 ]]; then
-    printf "Please install LLVM, LLVM-dev, and mentioned missing packages for build to continue.\nExiting...\n"
+    printf "Please install llvm, llvm-devel, and mentioned missing packages for build to continue.\nExiting...\n"
     exit -1
 fi
 
 # The overarching-goal of the below from-source builds is to 
 # provide an out-of-the-box solution that's self-contained for current and future static linkages for Clyth.
-mkdir -p libs_build/
-cd libs_build/
 
-if [[ ! -d musl ]]; then 
-    # Clone and build musl libc to use as the libc-of-choice for building LLVM's libc++
-    (
-        git clone https://git.musl-libc.org/git/musl
-    ) &
-fi
-if [[ ! -d llvm-project ]]; then
-    # Clone and build LLVM libc++ in the background while building musl lib
-    (
-        git clone https://github.com/llvm/llvm-project.git
-    ) &
-fi
-wait $(jobs -p)
-
-cd $CUR_DIR
-# podman pull alpine
-podman pull muslcc/x86_64:x86_64-linux-musl
-podman image prune -f
-
-if [[ ! -d llvm-libc++_build || ! -d musl_libc_build ]]; then
-    printf "\nFound podman container runtime - executing container build...\n"
-    podman build -f $CUR_DIR/Containerfile --tag clyth_alpine_container:1.0 -v $CUR_DIR/libs_build/:/home/libs_build/:Z --format docker 
-
-    if [[ $? != 0 ]]; then
-        printf "Error occurred during podman build call - exiting\n"
-        podman image prune -f
-        exit 1
-    fi
-    podman rmi localhost/clyth_alpine_container:1.0
-    podman container prune --force
-    podman image prune -f
-fi
 
 CMAKE_LISTS_FILE_PATH=./compiler-src/
 rm -rf build/
-cmake -S $CMAKE_LISTS_FILE_PATH -Bbuild
+cmake -S $CMAKE_LISTS_FILE_PATH -Bbuild -DCMAKE_C_COMPILER="zig cc" -DCMAKE_CXX_COMPILER="zig c++"
 cd build/
 make
